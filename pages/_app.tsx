dc38616d4775d9943ecaafcc3d5881dc30f60732
vincent <vincnttan@gmail.com>
2021-10-29 15:07:46 +0100

    feat: add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Nav from "../components/NavBar";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Nav />
       <Component {...pageProps} />
     </ChakraProvider>
   );
