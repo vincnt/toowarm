1f1e83d9d976e912b5e8670836a35eb377ed8d6b
vincent <vincnttan@gmail.com>
2021-11-05 14:47:11 +0000

    fix favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -103,7 +103,11 @@ const IndexPage = ({
     <>
       <Head>
         <title>Climate change in one page - It's Getting Too Warm</title>
-        <link rel="shortcut icon" href="/favicon.svg" />
+        {/* <link rel="shortcut icon" href="/favicon.svg" /> */}
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>&#129397;</text></svg>"
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
