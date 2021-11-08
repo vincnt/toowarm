36e839207336a3433dd9c574b8b31ba7675b78ea
vincent <vincnttan@gmail.com>
2021-11-08 14:31:38 +0000

    add meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -111,6 +111,12 @@ const IndexPage = ({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>&#129397;</text></svg>"
         />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Climate change summary. Evidence and predictions for climate change. How individuals and governments can help fight climate change."
+        />
+
+        <html lang="en" />
       </Head>
       <style jsx global>{`
         body {
