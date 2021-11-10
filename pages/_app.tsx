464ef699c91becdb29b1256056efa0faffd8a9a4
vincent <vincnttan@gmail.com>
2021-11-10 13:42:03 +0000

    add advanced gtag from github example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
-import Nav from "../components/NavBar";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-DS84DW5BVS`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
@@ -18,7 +32,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-DS84DW5BVS', {
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
