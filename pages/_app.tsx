0e3baca63b26dc21a65b9420c311b8d80c1b0d94
vincent <vincnttan@gmail.com>
2021-11-10 13:38:36 +0000

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,33 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Nav from "../components/NavBar";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      {/* <Nav /> */}
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-DS84DW5BVS`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DS84DW5BVS', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 export default MyApp;
